1a68f2a7c75f0e6147dbca01cc61bbbe88682f68
MK1 <shin.m.dp@gmail.com>
2023-06-20 01:11:55 +0300

    create app

---
+++ TextFields/Main/MainController.swift
@@ -0,0 +1,15 @@
+//
+//  MainController.swift
+//  TextFields
+//
+//  Created by SHIN MIKHAIL on 20.06.2023.
+//
+
+import UIKit
+import SnapKit
+
+class MainController: UIViewController {
+    override func viewDidLoad() {
+    
+    }
+}
